060fc072892e2d0c0b610ab4f5a1a476c8c7c177
Connor Irwin <connor.irwin@uoit.net>
2015-10-28 09:32:19 -0400

    adding files to git

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,9 @@
+has_no_e()
+    get inputstring ""
+    for i in length(inputstring)
+        if inputstring (i) == "e"
+            then print (False)
+	else
+	    print (True)
+
+has_no_e(gadsby_stripped.txt)
---
+++ Midterm/strip.sh
@@ -0,0 +1,3 @@
+#!
+#Connor Irwin - 100519748
+head -n -26 gadsby.txt|tail -n +2|
